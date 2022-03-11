de17b6c70c7654a46d1eb9acc8f1f9b65b7e57e0
Chola1 <99239237+Chola1@users.noreply.github.com>
2022-03-11 10:58:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var num = "All is "
+
+var str= "Skill Safari"
+
+var float="2.6"
+
+var boolean="True"
+
+console.log(num +"Well")
+console.log(str.length)
+console.log(Math.ceil(float))
+console.log(5+2==9)
+
+
+var r=10
+let pi=3.14
+const A=(r*r)*pi
+console.log("Area Of Circle is "+ A)
+
+var r=10
+let p=3.14
+const B=2*p*r 
+console.log("Circumference of Circle "+ B)
+
+const R= B/(2*pi)
+console.log("Radius Of Circle " +R)
